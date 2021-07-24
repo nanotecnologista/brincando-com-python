3c3dc8e98aebc1a3f36bd2170199a06171b9670a
Juju Nanotech <nanotecnologista@users.noreply.github.com>
2021-07-24 18:39:31 -0300

    Mudei a lógica do Bot

--- telegramBot.py
+++ telegramBot.py
@@ -2,10 +2,10 @@ import telebot
 import my_infos
 
 token = my_infos.token
-bot = telebot.TeleBot(token)
-
-@bot.message_handler(commands=["start"])
-def diga_oi(message):
-    chat_id = message.chat.id
-    bot.send_message(chat_id, "Olá, eu sou a J.B.O.T, a auxiliar da dr. Júlia, nanotecnologista.")
-bot.polling()
+class Method():
+    """Tentanto criar o método do meu jeito"""
+    def __init__(self):
+        self.token= my_infos.token
+        self.apitelegram = f"https://api.telegram.org/bot{self.token}"
+        self.headers = {'content-type': 'application/json', 'Cache-Control': 'no-cache'}
+        @staticmethod
---
+++ testandoAudio.py
@@ -0,0 +1,7 @@
+import os
+
+# Selecionando arquivos para abrir
+os.system("start cinderela.mp3") #único que serviu, usando apenas uma lib -- porém ele abre o app mesmo
+# os.system("afplay cinderela.mp3") 
+# p= vlc.MediaPlayer("file: ./cinderela.mp3")
+# p= play()
